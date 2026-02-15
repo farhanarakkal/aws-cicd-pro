720d5cad4f055942154c685fb8bf42f87f17ad7d
farhanarakkal <lionelfarhanarakkal@gmail.com>
2026-02-15 04:17:07 +0000

    updated message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Working from CloudShell 🚀");
+  res.send("CI/CD Working from CloudShell done by Farhan 🚀");
 });
 
 app.listen(3000, () => console.log("Server started"));
